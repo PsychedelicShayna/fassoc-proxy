8fe1f967b31b407a58041cb7b5488adc56703dfc
PsychedelicShayna <0x0000@tuta.io>
2022-08-23 17:23:22 -0300

    Switched to WinAPI for process creation, and other small changes.
    The main highlight of this commit is the switch to WinAPI for creating processes,
    as opposed to Rust's standard built-in std::process::Command, which fixes the various
    process launching issues, especially with cmd/pwsh, and also allowing for more fine
    grain control over how processes are launched, with the possibility of adding new
    JSON keys in the future.
    
    Also improved the code structure, renamed variables, and just made the codebase
    more consistent with itself.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] 
 #![cfg_attr(debug_assertions, windows_subsystem = "console")]
 
-use std::time::Duration;
 use serde_json as sj;
 use std::path::Path;
-use std::io::Write;
+use std::io::{Read, Write};
 use regex::Regex;
 use std::env;
 use std::fs;
+
+pub mod winutil;
 
 struct FileLogger;
 static FILE_LOGGER: FileLogger = FileLogger;
@@ -48,19 +49,21 @@ impl log::Log for FileLogger {
     fn flush(&self) {}
 }
 
-fn read_json_config(path: String) -> Result<sj::Value, String> {
-    let cfg_raw_json: String = match std::fs::read_to_string(path) {
+fn read_rules_json(path: String) -> Result<sj::Value, String> {
+    let cfg_raw_json: String = match std::fs::read_to_string(path.to_owned()) {
         Ok(raw_json) => raw_json,
-        Err(error) => return Err(format!("Failed to read config file: {:?}", error))
+
+        Err(error) => return 
+            Err(format!("Failed to read proxy rules file at \"{}\", because {}", path.to_owned(), error))
     };
 
     let parsed_json: sj::Value = match serde_json::from_str(cfg_raw_json.as_str()) {
         Ok(value) => value,
-        Err(error) => return Err(format!("Failed to parse the JSON data: {:?}", error))
+        Err(error) => return Err(format!("Failed to parse the proxy rules JSON: {:?}", error))
     };
 
     if !parsed_json.is_array() {
-        return Err(String::from("The parsed JSON data was not an array!"));
+        return Err(String::from("The parsed proxy rules JSON data was not an array!"));
     }
 
     return Ok(parsed_json);
@@ -76,21 +79,11 @@ fn main() {
     }
 
     let cli_args: Vec<String> = env::args().collect();
-    let usage = "Usage: fassoc-proxy <file-to-open> (optional: proxyrules.json)\nIf proxyrules.json not provided, environment variable FASSOC_PROXY_RULES is used to find proxyrules.json instead.";
 
     log::debug!("Received command line arguments: {:?}", cli_args);
  
     if cli_args.len() < 2 {
-        println!("{}", usage);
-        log::debug!("Printing usage and exiting due to invalid argument length, was < 2 ({:?})", cli_args.len());
-        std::process::exit(1);
-    }
-
-    // The same as if the argument were to fail with an incorrect value, but
-    // faster because it skips opening the file.
-    if cli_args[1].eq("-h") || cli_args[1].eq("--help") {
-        println!("{}", usage);
-        log::debug!("Printing usage and exiting because first argument was -h/--help");
+        log::error!("Exiting because the argument length {} < 2", cli_args.len());
         std::process::exit(1);
     }
 
@@ -104,72 +97,72 @@ fn main() {
         }
     };
 
-    let proxy_config_file_path: String = if cli_args.len() >= 3 {
+    let proxy_rules_path: String = if cli_args.len() >= 3 {
         cli_args[2].clone()
     } else {
         match env::var("FASSOC_PROXY_RULES") {
-            Ok(config) => config,
+            Ok(val) => val,
             Err(_) => {
-                log::error!("No argument or environment variable was given that points to the config file.");
+                log::error!("No argument or environment variable was given that points to the proxy rules file.");
                 panic!()
             }
         }
     };
 
-    let proxy_config_json = match read_json_config(proxy_config_file_path) {
-        Ok(config) => config,
+    let proxy_rules_json = match read_rules_json(proxy_rules_path) {
+        Ok(val) => val,
         Err(err) => {
-            log::error!("read_json_config returned err: {}", err);
+            log::error!("read_rules_json eeturned err: {}", err);
             panic!();
         }
     };
 
-    for (index, config_entry) in proxy_config_json.as_array().unwrap().iter().enumerate() {
-        // Collect the command string from the current config entry.
-        let mut command: String = match config_entry["command"].as_str() {
+    for (index, proxy_rule) in proxy_rules_json.as_array().unwrap().iter().enumerate() {
+        // Get the comment string from the current proxy rule.
+        let mut command: String = match proxy_rule["cmd"].as_str() {
             Some(val) => String::from(val),
             None => {
-                log::error!("Proxyrules JSON entry ({}) is missing \"command\" key.", index);
+                log::error!("Proxy rule #{} is missing the \"cmd\" key, or it is not a string.", index);
                 panic!();
             }
         };
 
-        // Collect the command argument string from the current config entry.
-        let mut command_args: Vec<String> = match config_entry["arguments"].as_array() {
-            Some(val) => val.to_owned().iter().map(|command_arg| {
-                match command_arg.as_str() {
-                    Some(command_arg) => String::from(command_arg),
-                    None => { 
-                        log::error!("One of the command arguments of config entry #{} is not a string!", index);
-                        panic!()
-                    }
-                }
-            }).collect(),
-
-            None => { 
-                log::error!("Config entry {:?} is missing arguments key.", index);
-                panic!()
+        // Get arguments string from proxy rule. This should be one contiguous
+        // string arguments, as opposed to a more traditional array approach.
+        let mut arguments: String = match proxy_rule["args"].as_str() {
+            Some(val) => val.to_owned(),
+            None => {
+                log::error!("Proxy rule #{} is missing the \"args\" key, or it is not a string.", index);
+                panic!();
             }
         };
 
-        // Collect the pattern strings from the current config entry.
-        let patterns: Vec<String> = match config_entry["patterns"].as_array() {
+        // Get working directory override from proxy rule, if the key it exists.
+        // If the key doesn't exist, get None instead, as this key is optional.
+        let mut wd_override: Option<String> = match proxy_rule["wd"].as_str() {
+            Some(val) => Some(String::from(val)),
+            None => None
+        };
+
+        // Get the RegEx pattern strings from the proxy rule.
+        let patterns: Vec<String> = match proxy_rule["regex"].as_array() {
             Some(val) => val.to_owned().iter().map(|pattern| {
                 match pattern.as_str() {
                     Some(pattern) => String::from(pattern),
                     None => { 
-                        log::error!("One of the patterns of config entry #{} is not a string!", index);
+                        log::error!("One of the RegEx patterns of proxy rule #{} is not a string!", index);
                         panic!()
                     }
                 }
             }).collect(),
             None => { 
-                log::error!("Config entry {:?} is missing patterns key.", index);
+                log::error!("Proxy rule #{} is missing the \"RegEx\" key, or it is not an array.", index);
                 panic!()
             }
         };
 
-        // Skip the current config entry if the target file doesn't match any of the entry's patterns.
+        // Skip the current proxy rule if the target file's name doesn't match 
+        // any of the proxy rule's RegEx patterns.
         if !patterns.iter().any(|pattern| {
             match Regex::new(pattern) {
                 Ok(val) => val.is_match(target_file.to_str().unwrap()),
@@ -182,38 +175,41 @@ fn main() {
             continue
         }
 
-        // Go through the command line arguments, and check if the command and its
-        // arguments contain a substitution string that should be substituted with
-        // a command line argument whose index matches the number in the string.
-        // If so, replace the string with the corresponding command line argument.
+        // Replace any valid palceholder strings in the command string and the
+        // argument strings with the appropriate values from the command line.
         for (index, cli_arg) in cli_args.iter().enumerate() {
-            // The substitution string for the current index.
-            let sstr: String = format!("~~$#{}", index);
+            let placeholder_lftm = format!("~~${}", index);
+            let placeholder: &str = placeholder_lftm.as_str();
 
-            // Substitute the substitution string (lookfor) in the command string.
-            if command.contains(sstr.as_str()) {
-                command = command.replace(sstr.as_str(), cli_arg);
+            if command.contains(placeholder) {
+                command = command.replace(placeholder, cli_arg);
             }
 
-            for command_arg in command_args.iter_mut()
-                                           .filter(|e|e.contains(sstr.as_str()))
-                                                  
-            {
-                *command_arg = command_arg.replace(sstr.as_str(), cli_arg)
+            if arguments.contains(placeholder) {
+                arguments = arguments.replace(placeholder, cli_arg);
+            }
+            
+            wd_override = match wd_override {
+                Some(val) => Some(val.replace(placeholder, cli_arg)),
+                None => None
             }
         }
 
-        match std::process::Command::new(command.as_str()).args(command_args.to_owned())
-                                                          .current_dir(target_folder)
-                                                          .output()
-        {
-            Ok(_) => {
-                log::debug!("Ran command \"{}\" with arguments ({:?}), in CWD: {}", 
-                    command, command_args, target_folder.to_str().unwrap());
-            },
-            Err(_) => ()
-        }
+        log::debug!(
+            "Creating process, path: \"{}\", args: \"{}\", cwd \"{}\"",
+            command,
+            arguments,
+            match wd_override.to_owned() {
+                Some(val) => val,
+                None => String::from("None")
+            }
+        );
 
-        std::thread::sleep(Duration::from_millis(2000));
+        winutil::create_process(command, arguments, wd_override);
+
+        if cfg!(debug_assertions) {
+            write!(std::io::stdout(), "{}", "Press enter to close the debug message console...").unwrap();
+            std::io::stdin().read(&mut [0u8]).unwrap();
+        }
     }
 }
